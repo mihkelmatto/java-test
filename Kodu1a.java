9dbb06b3f3e2f7b2888ad6d8251cfbde0fa3dc99
Mihkel <211209697+mihkelmatto@users.noreply.github.com>
2026-02-21 13:55:57 +0200

    3.9ms

--- Kodu1a.java
+++ Kodu1a.java
@@ -1,79 +1,72 @@
 //2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.
-// check: 2, 3, 5, 9, 16
-
 // algarvu kontroll kuni ruutjuur x
 
-// algarvu ring ei ole, kui
-// sisaldab arve 0, 2, 4, 5, 6, 8
-// kontrollid sisendit ennast, kui ta algab suurima numbriga
 /*
- 1. sisend paaris > paaritu
- 2. iga paaritu arvu ja rotatsioonide kontroll: test <= n, isprime, vastasel juhul katkestab ja võtab järgmise paaritu arvu
- 3. kui algarvuringi max ei ole veel primesets listis, siis lisab sinna
-*/
-
+ 1. algarvude leidmine
+ 2. iga algarvu n -> 9 puhul kontroll, kas pöörded on algarvud
+ 3. kui kõik pöörded on algarvud, siis:
+        primes[algarvud] = false duplikaatide eemaldamiseks
+        lisada vastustesse suurim algarv (tsükli i)
+ */
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Kodu1a {
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        algarvuRingid5Suurimat(1000000);
-        algarvuRingideArv(1000000);
-        long endTime = System.nanoTime();    // lõpp
-        long duration = (endTime - startTime) / 1000; // kestus nanosekundites
-        System.out.println(duration);
+        int input = 100;
+
+        algarvuRingid5Suurimat(input);
+        algarvuRingideArv(input);
     }
 
     static void algarvuRingid5Suurimat(int n){
         ArrayList<Integer> primesets = getprimesets(n);
-        Collections.sort(primesets, Collections.reverseOrder());
-        // primesets.subList(5, primesets.size()).clear();
+
         System.out.printf("Antud lähtekoht: %d\n", n);
-        for(int i=0; i < Math.min(5, primesets.size()); i++){
+        for(int i = 0; i<Math.min(5, primesets.size()); i++){
             System.out.println(primesets.get(i));
         }
-        System.out.printf("Väiksemaid erinevaid algarvuringe: %d\n", algarvuRingideArv(n) - 5);
+        System.out.printf("Väiksemaid erinevaid algarvuringe: %d\n", algarvuRingideArv(n));
     }
 
     static int algarvuRingideArv(int n){
         ArrayList<Integer> primesets = getprimesets(n);
-        int arv = primesets.size();
-        return arv;
+        return primesets.size();
     }
 
-    static ArrayList<Integer> getprimesets(int num){
+    static ArrayList<Integer> getprimesets(int n){
+        boolean[] primes = getprimes(n);
+        if(n % 2 == 0) n-=1; // paaris > paaritu
+
         ArrayList<Integer> primesets = new ArrayList<>();
 
-        // proovib kõiki paarituid arve vahemikus [10, num] ja salvestab maksimaalsed väärtused primesets listi
-        if(num % 2 == 0) num-= 1; // paaris > paaritu
-        for(int i = num; i > 9; i -= 2){
-            int testnum = i;                                // kirjutan üle rotatsioonide ajal, i puhul vist teeks katki
-            int numlen = (int)(Math.log10(testnum)) + 1;    // arg for rotate()
-            int maxitem = 0;
-            boolean validnum = true; // isprime() && i<=num
+        // iga algarv suuremast väiksemani (paaritu i)
+        for(int i = n; i > 9; i-=2){
 
-            // rotatsioonide testimine kuni üks arv ei sobi (validnum) või jõuab lõpuni
-            for(int j = 0; j<numlen; j++){ // kas intervall on õige? #########
-                if(testnum <= num && numval(testnum) && isprime(testnum)){
-                    if(testnum > maxitem) maxitem = testnum;
-                    testnum = rotate(testnum, numlen); // roteerib 1x liiga palju
+            if(primes[i]){
+                int numlen = (int)Math.log10(i) + 1;
+
+                // pöörded
+                int testnr = i;
+                boolean valid = true;
+
+                // Kontrollib, kas pöörded on algarvud ja lisab sobivusel testseti
+                for(int j = 0; j < numlen-1; j++){
+                    testnr = rotate(testnr, numlen);
+                    if(testnr <= n && primes[testnr]){ // filtreerib <=n
+                        primes[testnr] = false;
+                    }
+                    else{
+                        valid = false;
+                        break;
+                    }
                 }
-                else{
-                    validnum = false;
-                    break;
+                if(valid){
+                    primesets.add(i);
                 }
             }
-            if(validnum && !primesets.contains(maxitem)) primesets.add(maxitem);
         }
         return primesets;
-    }
-
-    static boolean numval(int num){
-        String numstr = String.valueOf(num);
-        if(num % 3 == 0) return false;
-        if(numstr.matches(".*[024568].*")) return false;
-        return true;
     }
 
     static int rotate(int num, int numlength){ // length juhuks, kui num lõpeb nulliga
@@ -85,12 +78,23 @@ public class Kodu1a {
         return newstart + newend;
     }
 
-    static boolean isprime(int num){ // sisend on alati paaritu arv
-        int limit = (int)(Math.sqrt(num));
-        if(num == 1) return false;
-        for(int i=3; i<=limit; i+=2){
-            if(num % i == 0) return false;
+    static boolean[] getprimes(int n){
+        boolean[] primes = new boolean[n+1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        int limit = (int)Math.sqrt(n);
+
+        for(int i=2; i<=limit; i++){
+            if(primes[i]){
+                int temp = i * i;
+                while(temp <= n){
+                    primes[temp] = false;
+                    temp += i;
+                }
+            }
         }
-        return true;
+        return primes;
     }
+
 }
